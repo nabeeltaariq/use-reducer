36d7d3d0fe96e6cd7498ae1831a7723ccbbc7f98
nabeeltaariq <nabeeltaariq@gmail.com>
2021-01-30 09:14:36 -0800

    Committed to git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useReducer } from 'react'
+import './App.css'
+import counterReducer from './counter.reducer'
 function App() {
+  let [state, dispatch] = useReducer(counterReducer, 0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>This is the value of State {state}</h3>
+
+      <button onClick={() => dispatch('INCREMENT')}>INCREMENT </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
